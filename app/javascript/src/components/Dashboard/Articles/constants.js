d5d231ded2613dd6ade54928ada46b0280234309
Devu R Kurup <40559138+devukurup@users.noreply.github.com>
2023-06-13 00:52:39 +0530

    Removed active key from statuses constant

--- app/javascript/src/components/Dashboard/Articles/constants.js
+++ app/javascript/src/components/Dashboard/Articles/constants.js
@@ -3,17 +3,14 @@ import { t } from "i18next";
 export const STATUSES = [
   {
     label: t("statuses.all"),
-    active: true,
     count: 10,
   },
   {
     label: t("statuses.published"),
-    active: false,
     count: 20,
   },
   {
     label: t("statuses.draft"),
-    active: false,
     count: 30,
   },
 ];
