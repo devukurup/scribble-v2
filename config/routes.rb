06fd83be80bb8471fef2a62fe3d2172d695c7069
Devu R Kurup <40559138+devukurup@users.noreply.github.com>
2023-06-14 03:50:13 +0530

    Defined routes for categories controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  constraints(lambda { |req| req.format == :json }) do
+    resources :categories, only: %i[index create update]
+  end
+
   root "home#index"
   get "*path", to: "home#index", via: :all
 end
