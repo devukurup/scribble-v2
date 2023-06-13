84cf3c235ad4fc3db130e6e4cef181b356bace4a
Devu R Kurup <40559138+devukurup@users.noreply.github.com>
2023-06-13 18:05:41 +0530

    Added a User association to Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,7 @@
 class Category < ApplicationRecord
   MAX_TITLE_LENGTH = 255
 
+  belongs_to :user
+
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: MAX_TITLE_LENGTH }
 end
