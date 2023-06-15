148561ff3dfd6cd87a274d333eb877984466cca2
Devu R Kurup <40559138+devukurup@users.noreply.github.com>
2023-06-16 02:49:29 +0530

    Added title and description to manage categories UI

--- app/javascript/src/components/Dashboard/Settings/ManageCategories/index.jsx
+++ app/javascript/src/components/Dashboard/Settings/ManageCategories/index.jsx
@@ -1,5 +1,48 @@
 import React from "react";
 
-const ManageCategories = () => <div>ManageCategories</div>;
+import { Plus } from "neetoicons";
+import { Button, Typography } from "neetoui";
+import { useTranslation } from "react-i18next";
+
+const ManageCategories = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="m-32 flex w-full flex-col border-2 p-5">
+      <Typography
+        className="neeto-ui-text-gray-700"
+        lineHeight="loose"
+        style="h2"
+        weight="semibold"
+      >
+        {t("settings.manageCategories.title")}
+      </Typography>
+      <Typography
+        className="neeto-ui-text-gray-600"
+        lineHeight="normal"
+        style="body1"
+        weight="normal"
+      >
+        {t("settings.manageCategories.description")}
+      </Typography>
+      <div className="my-4 flex items-center justify-between">
+        <Typography
+          className="neeto-ui-text-gray-600"
+          style="body2"
+          weight="normal"
+        >
+          {t("settings.manageCategories.count", { count: 7 })}
+        </Typography>
+        <Button
+          icon={Plus}
+          iconPosition="left"
+          label={t("category.add")}
+          size="medium"
+          style="link"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default ManageCategories;
