2f50ddf18335a3ce5cd6fc63d40f7c671574cc4d
Devu R Kurup <40559138+devukurup@users.noreply.github.com>
2023-06-18 03:38:11 +0530

    Added article model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,12 @@
 
 class Category < ApplicationRecord
   MAX_TITLE_LENGTH = 255
+  VALID_TITLE_REGEX = /\A[a-zA-Z][a-zA-Z0-9& -]*\z/
 
   acts_as_list scope: :user
 
   belongs_to :user
 
-  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: MAX_TITLE_LENGTH }
+  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: MAX_TITLE_LENGTH },
+    format: { with: VALID_TITLE_REGEX }
 end
