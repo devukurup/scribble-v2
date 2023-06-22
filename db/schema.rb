4acf618406fe0838530955f46689187e74d1ce4a
Devu R Kurup <40559138+devukurup@users.noreply.github.com>
2023-06-23 00:02:01 +0530

    Added organizations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_17_180812) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_22_175131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -38,6 +38,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_180812) do
     t.integer "position"
     t.index ["title"], name: "index_categories_on_title", unique: true
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "organizations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_organizations_on_name", unique: true
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
