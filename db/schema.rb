58c241536b498f282e308de122d0cba876170a93
Devu R Kurup <40559138+devukurup@users.noreply.github.com>
2023-07-14 15:05:45 +0530

    Added papertrail gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_220110) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_174315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -72,6 +72,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_220110) do
     t.uuid "site_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["site_id"], name: "index_users_on_site_id"
+  end
+
+  create_table "versions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "item_type", null: false
+    t.uuid "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "articles", "categories"
