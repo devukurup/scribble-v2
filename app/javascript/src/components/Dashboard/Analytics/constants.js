cea08f64886cae81f2641a4237d51a3e5fc8b645
Devu R Kurup <40559138+devukurup@users.noreply.github.com>
2023-07-22 00:07:41 +0530

    Moved render methods to utils

---
+++ app/javascript/src/components/Dashboard/Analytics/constants.js
@@ -0,0 +1,40 @@
+import { t } from "i18next";
+
+import { renderFormattedDate, renderTitleLink } from "./utils";
+
+export const COLUMN_DATA = [
+  {
+    key: "title",
+    dataIndex: "title",
+    title: t("common.title"),
+    width: 200,
+    render: (title, { slug }) => renderTitleLink({ title, slug }),
+  },
+  {
+    key: "createdAt",
+    dataIndex: "createdAt",
+    title: t("analytics.table.createdAt"),
+    render: createdAt => renderFormattedDate(createdAt),
+    width: 50,
+  },
+  {
+    key: "category",
+    dataIndex: "category",
+    title: t("analytics.table.category"),
+    width: 100,
+  },
+  {
+    key: "visitCount",
+    dataIndex: "visitCount",
+    title: t("analytics.table.visits"),
+    width: 60,
+    sorter: true,
+  },
+];
+
+export const SORT_OPTIONS = {
+  descend: "desc",
+  ascend: "asc",
+};
+
+export const DEFAULT_SORT_OPTION = SORT_OPTIONS.descend;
